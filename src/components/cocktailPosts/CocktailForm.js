a7c6f7397521df9cf1ede47ea1c78b2e32bdfca1
Ameena Mashaikh <ameenamashaikh@gmail.com>
2022-12-09 15:44:34 -0500

    select dropdowns for ingredients

--- src/components/cocktailPosts/CocktailForm.js
+++ src/components/cocktailPosts/CocktailForm.js
@@ -1,5 +1,6 @@
 import { useEffect, useState, useCallback } from "react"
 import { useParams, useNavigate, Link } from "react-router-dom"
+import Select from "react-select"
 import { getLiquors, getLiqueurs, getStapleIngredients } from "../../managers/IngredientManager"
 import "./CocktailPosts.css"
 
@@ -25,30 +26,83 @@ export const CocktailPostForm = () => {
     
     
     
-        return (
+    return (
         <form className="cocktailPostForm">
             <div className = "ingredient_list">
                 <div className = "liquor_select">
-                    <select
-                        required autoFocus
-                        className = "form-control"
-                        value = {liquors.id}
-                        // onChange = {(event) => {
-                        //     const valueParse = (event.target.value)
-                        //     liquors.map(liquor => {
 
-                        //     })
-                        // }}
-                        >
-                        <option value = "0">Select Liquors</option>
-                        {
-                            liquors.map(liquor => {
-                                return <option key = {liquor.id} value = {liquor.id}> {liquor.name}</option>
+                    <Select
+                        placeholder = "Select Liquors"
+                        options ={
+                            liquors.map((liquor) => {
+                                return {
+                                    label: liquor.label,
+                                    value: liquor.id
+                                }
                             })
                         }
-                    </select>
+                        isMulti
+                        isSearchable = {true}
+                    />
+                </div>
+                <div className = "liqueur_select"> 
+                <Select
+                        placeholder = "Select Liqueurs"
+                        isMulti
+                        options ={
+                            liqueurs.map((liqueur) => {
+                                return {
+                                    label: liqueur.name,
+                                    value: liqueur.id
+                                }
+                            })
+                        }
+                        />
+                        
+                </div>
+                <div className = "staple_ingredient_select"> 
+                <Select
+                        placeholder = "Select Staple Ingredients"
+                        isMulti
+                        options ={
+                            stapleIngredients.map((staple) => {
+                                return {
+                                    label: staple.name,
+                                    value: staple.id
+                                }
+                            })
+                        }
+                        />
+                        
                 </div>
             </div>
         </form>            
     )
 }
+                        
+
+
+
+
+
+
+
+
+                        {/* <select
+                            required autoFocus
+                            className = "form-control"
+                            value = {liquors.id}
+                            // onChange = {(event) => {
+                            //     const valueParse = (event.target.value)
+                            //     liquors.map(liquor => {
+    
+                            //     })
+                            // }}
+                            >
+                            <option value = "0">Select Liquors</option>
+                            {
+                                liquors.map(liquor => {
+                                    return <option key = {liquor.id} value = {liquor.id}> {liquor.name}</option>
+                                })
+                            }
+                        </select> */}
